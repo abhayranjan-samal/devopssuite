515357acb0159daa3e2459ec590c087dd705da6b
sudo <sudo@sudos-MacBook-Air.local>
2025-08-03 11:01:29 +0530

    added react files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is your minimal React app.</p>
+    </div>
+  );
+}
+
+export default App;
